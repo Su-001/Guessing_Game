05ca293a80f0574fd81fcc561dd4ee0f22ebad64
Su-001 <110022483+Su-001@users.noreply.github.com>
2022-08-14 15:24:31 +0530

    Added hints and name

--- main.py
+++ main.py
@@ -3,15 +3,29 @@ guess = " "
 guess_count = 0
 guess_limit = 3
 out_of_guesses = False
+name = input("ENTER YOUR NAME : ")
+print("hint : it's an animal ")
+
 while guess != secret_word and not out_of_guesses:
-    if guess_count < guess_limit:
+    if guess_count < guess_limit-1:
         guess = input("enter guess: ")
         guess_count += 1
+
+    elif guess_count <guess_limit :
+         print("want hints? Y/N ")
+         hints =input("Y/N")
+         if hints == "Y" or "y":
+            print("Hint : three letter word")
+            guess = input("enter guess: ")
+            guess_count += 1
+         else :
+            guess = input("enter guess: ")
+            guess_count += 1
     else:
         out_of_guesses = True
 
 
 if out_of_guesses :
-    print("you are out of guesses, you lose! ")
+    print(name +  "are out of guesses," +name+ " lose! ")
 else:
-    print("you win")
+    print(name + " win")
